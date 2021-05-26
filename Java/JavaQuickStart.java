4c04f4e5b3063968aa7171c66bd64f108e84a735
Peyman27m <63470748+Peyman27m@users.noreply.github.com>
2021-05-26 17:25:21 +0430

    Update JavaQuickStart.java

--- Java/JavaQuickStart.java
+++ Java/JavaQuickStart.java
@@ -1,7 +1,7 @@
 HttpRequest request = HttpRequest.newBuilder()
 		.uri(URI.create("https://face-mask-detection.p.rapidapi.com/FaceMaskDetection"))
 		.header("content-type", "application/x-www-form-urlencoded")
-		.header("x-rapidapi-key", "f485496ab8msh2ea068a0bbe333cp179f86jsn2b4afe8962dd")
+		.header("x-rapidapi-key", "APIKEY")
 		.header("x-rapidapi-host", "face-mask-detection.p.rapidapi.com")
 		.method("POST", HttpRequest.BodyPublishers.ofString("linkFile=%3CREQUIRED%3E"))
 		.build();
